c25120ed5a0698dc0e2ecbf8ee56e10302d95d4c
eunbileeme <pask2202@gmail.com>
2023-11-27 02:48:23 +0900

    [feat] 잃어버린 괄호

---
+++ src/silver/greedy/LostParentheses.java
@@ -0,0 +1,38 @@
+package silver.greedy;
+
+import java.io.*;
+
+// 최소값 -> 음수값이 커야 -> 최대한 큰 수를 빼주어야 => 덧셈 부분을 먼저 계산해야 = "뺄셈을 기준으로 분리"
+public class LostParentheses {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // ※ 수는 5자리를 초과할 수 없으며, 연속으로 tmp에 같은 값이 들어올 수 있으므로
+        // 중간 계산 결과값으로 나올 수 있는 0이 아닌 값으로 초기화
+
+        // ※ 이 부분에 대한 예시 추가 예정
+        int sum = Integer.MAX_VALUE;
+
+        // [30, 70, 20+40, 10+100+30, 35]
+        String[] sub = br.readLine().split("-");
+
+        for (int i = 0; i < sub.length; i++) {
+            int tmp = 0;
+
+            // ※ +의 경우, 정규식이 아닌 메타 문자이므로
+            // 문자 그 자체를 표현하기 위해 이스케이프 처리(\\) 진행
+
+            // i = 0, [30] -> i = 1, [70], i = 2 -> [20, 40], ..
+            String[] add = sub[i].split("\\+");
+
+            for (int j = 0; j < add.length; j++) {
+                // j = 0, + 20 -> j = 1, + 20 + 40
+                tmp += Integer.parseInt(add[j]);
+            }
+
+            if (sum == Integer.MAX_VALUE) sum = tmp;
+            else sum -= tmp;
+        }
+
+        System.out.println(sum);
+    }
+}
