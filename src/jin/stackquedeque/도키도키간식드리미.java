12b558e5805db1226cf2d0278efb87744ef4c496
jinhyuck <gjwlsgur4866@gmail.com>
2023-12-02 17:13:36 +0900

    [S3] 12789. 도키도키 간식드리미 수정

--- src/jin/stackquedeque/도키도키간식드리미.java
+++ src/jin/stackquedeque/도키도키간식드리미.java
@@ -4,7 +4,8 @@ import java.io.*;
 import java.util.*;
 
 // [S3] 12789. 도키도키 간식드리미
-// 17%정도쯤에서 틀림 -> 무엇이 문제일까?
+// 17%정도쯤에서 틀림 -> 무엇이 문제일까? -> 대기열이 비어있지 않은 상태에서 다음 사람이 대기열에 있는 경우를 배제했다!
+// 인덱스 관련해서 문제가 있는 것 같지만 어떤 문제인지 알 수가 없어서 que를 사용하자.
 // if (idx == target) pass else list.add(idx);
 // age = stack.pop()
 // while (!stack.isEmpty()):
@@ -12,33 +13,45 @@ import java.util.*;
 // if (now != age + 1) return false
 // else age = now
 public class 도키도키간식드리미 {
+    static int target = 1;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         final int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        List<Integer> stack = new ArrayList<>();
-
-        int target = 1;
-        int idx = 0;
-        while (idx < N) {
-            int data = Integer.parseInt(st.nextToken());
-            if (data == target) {
-                target++;
-            } else {
-                stack.add(data);
-            }
-            idx++;
+        Queue<Integer> que = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            que.add(Integer.parseInt(st.nextToken()));
         }
-
-        System.out.println(stack.isEmpty() ? "Nice" :
-                isGetSnack(stack, target) ? "Nice" : "Sad");
-        br.close();
+        List<Integer> anotherLine = new ArrayList<>();
+        makeAnotherLine(que, anotherLine);
+        System.out.println(isPerfectOrderAboutAnotherLine(anotherLine) ? "Nice" : "Sad");
     }
 
-    private static boolean isGetSnack(List<Integer> stack, int target) {
-        while (!stack.isEmpty() && stack.get(stack.size() - 1) == target) {
-            target = stack.remove(stack.size() - 1) + 1;
+    private static void makeAnotherLine(Queue<Integer> que, List<Integer> line) {
+        while (!que.isEmpty()) {
+            if (que.peek() == target) {
+                que.poll();
+                target++;
+            } else {
+                if (!line.isEmpty() && line.get(line.size() - 1) == target) {
+                    line.remove(line.size() - 1);
+                    target++;
+                } else {
+                    line.add(que.poll());
+                }
+            }
         }
-        return stack.isEmpty();
+    }
+
+    private static boolean isPerfectOrderAboutAnotherLine(List<Integer> line) {
+        while (!line.isEmpty()) {
+            int order = line.remove(line.size() - 1);
+            if (order == target) {
+                target++;
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 }
