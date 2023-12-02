b0f4187f08b1b5bee6e95820934f95e8a0982073
eunbileeme <pask2202@gmail.com>
2023-12-02 23:46:28 +0900

    [feat] 제로

---
+++ src/silver/stackqueuedeque/Zero.java
@@ -0,0 +1,34 @@
+package silver.stackqueuedeque;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Zero {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 1. 입력 변수 초기화
+        int k = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        // 2. 로직
+        for (int i = 1; i < k + 1; i ++) {
+            int element = Integer.parseInt(br.readLine());
+
+            if (element == 0) {
+                stack.pop();
+            } else {
+                stack.push(element);
+            }
+        }
+
+        // 3. 출력
+        int sum = 0;
+        for (Integer e : stack) {
+            sum += e;
+        }
+
+        System.out.println(sum);
+        br.close();
+    }
+}
