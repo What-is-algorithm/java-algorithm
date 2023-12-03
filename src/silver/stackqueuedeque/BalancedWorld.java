ed2ce979653dcd16b0eab97ef3703aaa997eeb62
eunbileeme <pask2202@gmail.com>
2023-12-03 11:21:19 +0900

    [feat] 균형잡힌 세상

---
+++ src/silver/stackqueuedeque/BalancedWorld.java
@@ -0,0 +1,46 @@
+package silver.stackqueuedeque;
+
+import java.io.*;
+import java.util.*;
+
+public class BalancedWorld {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String str;
+        while (!(str = br.readLine()).equals(".")) {
+            sb.append(balance(str)).append('\n');
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+
+    private static String balance(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (c == '(' || c == '[') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (stack.empty() || stack.peek() != '(') {
+                    return "no";
+                } else {
+                    stack.pop();
+                }
+            } else if (c == ']') {
+                if (stack.empty() || stack.peek() != '[') {
+                    return "no";
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        if (stack.empty()) return "yes";
+        else return "no";
+    }
+}
