289cd324a1bf4647537d0844c2d5432b6b118122
yelimkin <yelim785@gmail.com>
2023-12-13 00:51:02 +0900

    [feat] 회사에 있는 사람

---
+++ src/yelim/setmap/Peopleincompany.java
@@ -0,0 +1,40 @@
+package yelim.setmap;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Peopleincompany {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        // 동명이인 X(중복 X), 대소문자가 달라도 다른 이름
+        HashSet<String> set = new HashSet<>();
+        for(int i=0;i < n;i++) {
+            String[] log = br.readLine().split(" ");
+            if(log[1].equals("enter")) {
+                set.add(log[0]);
+            }else {
+                set.remove(log[0]);
+            }
+        }
+
+        List<String> lst = new ArrayList<>(set);
+        // 사전 순의 역순으로 출력하기 위해 정렬
+        Collections.sort(lst, Collections.reverseOrder());
+        for(String name : lst) {
+            bw.write(name);
+            bw.newLine();
+        }
+        br.close();
+        bw.close();
+    }
+}
