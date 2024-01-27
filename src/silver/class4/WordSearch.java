c8b68f02bef3ac16426841fd2f92b6205ecba4f8
eunbileeme <pask2202@gmail.com>
2024-01-27 18:21:14 +0900

    [feat] Word Search

---
+++ src/silver/class4/WordSearch.java
@@ -0,0 +1,14 @@
+package silver.class4;
+
+// Todo 2차원 배열을 하나씩 탐색하면서 word를 완성하기
+// Todo 단, 모든 칸을 탐색하면서 방문했던 위치는 다시 default 값으로 초기화를 해줘야 함
+// Todo 다시 탐색할 때, 해당 위치를 다시 탐색할 수 있어야 하기 때문
+
+// Todo 재귀를 활용한 백트래킹
+// Todo 이유는 ..
+class Solution {
+    public:
+    bool exist(vector<vector<char>>& board, string word) {
+
+    }
+};
