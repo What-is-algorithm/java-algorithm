8e8482b45bc585b3c8a1b76a6a6d4e967a49dfad
eunbileeme <pask2202@gmail.com>
2024-03-12 16:28:05 +0900

    [feat] Σ

---
+++ src/silver/class4/Sigma.java
@@ -0,0 +1,9 @@
+package silver.class4;
+
+import java.io.IOException;
+
+public class Sigma {
+    public static void main(String[] args) throws IOException {
+        // (2024.03.12) 문제 이해 중
+    }
+}
